05958862e856b814f648dce4629b2423cab2b90d
sharon <sharonvazana@gmail.com>
2021-04-08 20:19:29 +0300

    repository name change

--- aoop-part1/src/country/RamzorColor.java
+++ aoop-part1/src/country/RamzorColor.java
@@ -13,6 +13,6 @@ public enum RamzorColor {
 	public double getValue() {
         return this.value;
     }
-	
+		
 	private final double value;
 }
--- aoop-part1/src/country/Settlement.java
+++ aoop-part1/src/country/Settlement.java
@@ -1,12 +1,64 @@
 package country;
 
-public class Settlement {
-	public Settlement() {
-		
+import location.Location;
+import location.Size;
+
+public class Settlement {// abstract???
+	public Settlement(String name, Location location, Person[] people) {
+		m_name = name;
+		m_location = location;
+		m_people = people;
+		m_ramzorColor = RamzorColor.GREEN;	// default
 	}
-	
+
+//	public String toString() {
+//		return "width: " + m_width + ", height: " + m_height;
+//	}
+//	
+//	public boolean equals(Object o) {
+//		if (!(o instanceof Size))
+//			return false;
+//		Size s = (Size)o;
+//		return m_width == s.m_width && m_height == s.m_height;
+//		}
+
+	protected RamzorColor calculateRamzorGrade() {// calculates new color
+		// abstract??? protected???
+	}
+	/**
+	 * 
+	 * @return percentage of sick people in a certain range
+	 */
+	public double contagiousPercent() {// 0 to 1 max
+		//???
+	}
+	/**
+	 * 
+	 * @return random location in the settlement
+	 */
+	public Location randomLocation() {
+		//???
+	}
+	/**
+	 * 
+	 * @param p - new person to add
+	 * @return true if Person added successfully to settlement
+	 */
+	public boolean addPerson(Person p) {
+		//???
+	}
+	/**
+	 * 
+	 * @param p - person to transfer
+	 * @param s - new settlement to transfer Person into
+	 * @return true if successfully transferred
+	 */
+	public boolean transferPerson(Person p, Settlement s) {
+		return true; // for this part of the project
+	}
 	private String m_name;
 	private Location m_location;
-	private Person m_people;
-	private RamzorColor ramzorColor;
+	private Person[] m_people;
+	private RamzorColor m_ramzorColor;
+	
 }
