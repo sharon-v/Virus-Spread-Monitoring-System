366f3006abdfc22c5825137c235f72c5751fb1af
yarden hovav <hovav4321@gmail.com>
2021-04-10 17:42:55 +0300

    simulation class

--- aoop-part1/src/io/SimulationFile.java
+++ aoop-part1/src/io/SimulationFile.java
@@ -1,30 +1,73 @@
 package io;
 
-import country.Map;
-import country.Settlement;
+import java.io.*;
+import java.util.Random;
+
+import country.*;
+import location.*;
+import population.Healthy;
 import population.Person;
 
 public class SimulationFile {
-	public void ReadFromFile() {
-//		Map newMap = new Map();
-//		 read from the file one settlement and add to the map
-//		settlemntName
-//		settlementLocation
-//		settlementPopulationAmount
-//		Person [] population = new Person[settlementPopulationAmount];
-//		Person [] population = new Person[2];
-//		for(int i=0;i<settlementPopulationAmount;++i) {
-//			population[0] = new Person(RandomAge(), settlementLocation,??? )	
-//		}
-//		
-//		Settlement newSettlement = new Settlement(settlemntName,settlementLocation,population)//from the file
-//		newMap.addSettlement(newSettlement);
+	/**
+	 * Constractor
+	 */
+	public SimulationFile() {
+		
 	}
 	
+	public void ReadFromFile() {
+		try {
+			FileReader fr = new FileReader("settlement.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String settl = br.readLine();
+			while(br != null) {
+				String[] settlDeteails = settl.split(";");
+				String settlementType = settlDeteails[0];
+				String settlemntName = settlDeteails[1];
+				Point settlementPoint = new Point(Integer.parseInt(settlDeteails[2]), Integer.parseInt(settlDeteails[3]));//convert from string to integer
+				Size settlementSize = new Size(Integer.parseInt(settlDeteails[4]), Integer.parseInt(settlDeteails[5]));
+				Location settlementLocation = new Location(settlementPoint, settlementSize); //??
+				int settlementPopulationAmount = Integer.parseInt(settlDeteails[6]);
+				Person [] population = new Person[settlementPopulationAmount]; // create the population array
+				for(int i=0;i<settlementPopulationAmount;++i) 
+					population[0] = new Healthy(RandomAge(), settlementLocation,??? ); //??
+				Map newMap = new Map();
+				if(settlementType == "City")
+					newMap.addSettlement(new City(settlemntName, settlementLocation, population));
+				else if(settlementType == "Moshav")
+					newMap.addSettlement(new Moshav(settlemntName, settlementLocation, population));
+				else if(settlementType == "Kibbutz")
+					newMap.addSettlement(new Kibbutz(settlemntName, settlementLocation, population));
+				settl = br.readLine();
+			}
+		}
+		catch (FileNotFoundException e) {
+			throw e; //???? 
+		}
+		catch (IOException e) {
+			System.err.println(e.getMessage()); //????? 
+		}
+		finally {
+			br.close();
+			fr.close();
+		}
+	}
 	
-	
+	/**
+	 * 
+	 * @return A random age of person
+	 */
 	public int RandomAge() {
-		
+		 int standardDeviation = 6;
+		 int Mean = 9; //Average
+		 Random ran = new Random();
+		 // generating integer
+		 int x =(int)ran.nextGaussian()*standardDeviation + Mean; // random number for x by normal distribution
+		 int yMin = 0; //???
+		 int yMax = 4 ; //???
+		 int y = (int) Math.random()*(yMax - yMin +1) + yMin; // random number for y
+		 return (5*x + y);
 		
 	}
 }
