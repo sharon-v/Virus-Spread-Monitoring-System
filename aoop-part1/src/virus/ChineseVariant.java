1823f0e8d434e9b816734d09c2584ab6dcb30678
yarden hovav <hovav4321@gmail.com>
2021-04-11 13:25:27 +0300

    IVirus package

--- aoop-part1/src/virus/ChineseVariant.java
+++ aoop-part1/src/virus/ChineseVariant.java
@@ -28,19 +28,21 @@ public class ChineseVariant implements IVirus {
 	
 	@Override
 	public boolean tryToContagion(Person p1, Person p2){
+		double randonNumber = Math.random();
 		if(p2.healthCondition() != "Sick") {
 			double d = p1.distance(p2); // distance between 2 people
-			if(contagionProbability(p2) * Math.min(1, 0.14 * Math.exp(2 - 0.25 * d)) < 1)
-				return false; //??
+			if(contagionProbability(p2) * Math.min(1, 0.14 * Math.exp(2 - 0.25 * d)) > randonNumber)
+				return true; 
 			else 
-				return true;
+				return false;
 		}
-		throw new UnsupportedOperationException("You can't get sick twice man !!");
+		throw new UnsupportedOperationException("You can't get sick twice man !!"); //where to catch this
 //		return false;
 	}
 	
 	@Override
 	public boolean tryToKill(Sick s) {
+		double randonNumber = Math.random();
 		double p ; //the probability to die according to age
 		if(s.getAge() <= 18)
 			p = deathProbTo18;
@@ -49,8 +51,8 @@ public class ChineseVariant implements IVirus {
 		else 
 			p = deathProb55Above; 
 		long t = Clock.now() - s.getContagiousTime(); //the time that passed since contagion 
-		if(Math.max(0, p - 0.01 * p * Math.pow(t  -15, 2)) != 1)
-			return false; //???
-		return true;
+		if(Math.max(0, p - 0.01 * p * Math.pow(t  -15, 2)) > randonNumber)
+			return true; 
+		return false;
 	}
 }
